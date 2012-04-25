e700b97555fdd49965feb17394f36c65cd74322f
Konstantin Pribluda <konstantin.pribluda@gmail.com>
2012-04-25 21:38:11 +0200

    initial imports

---
+++ src/main/java/de/pribluda/google/youload/Youload.java
@@ -0,0 +1,17 @@
+package de.pribluda.google.youload;
+
+/**
+ * simple loader to youtube
+ *
+ * @author Konstantin Pribluda
+ */
+public class Youload {
+
+    /**
+     *  just upload file
+     * @param args user password filename
+     */
+    public static void main(String[] args) {
+
+    }
+}
